cafc45827eb00daf722386575a0ceccda384b877
kpandian <kpandian@ucsc.edu>
2020-08-21 18:43:46 -0700

    Learning how to use PDB

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import unittest
 
 
-def main():
-    return None
+def main(input):
+    return input.title()
 
+def main2(input):
+    if not input or len(input) == 0:
+        return ""
+    result = "" 
+    for word in input.split(" "):
+        if len(word) > 0:
+            result += word[0].upper() + word[1:] + " "
+        else:
+            result += " "
+   # print(result)
+    #return result
+    return result[0:len(result)-1]
+    #return " ".join(result)
+    
 if __name__ == '__main__':
-    main()
+    print(main("inputted string"))
+    print(main2("inputted string"))
+    print(main("i'm dumb"))
+    print(main2("i'm dumb"))
+    print(main(""))
+    print(main2(""))
+    print(main("Hello     World"))
+    print(main2("Hello     World"))
